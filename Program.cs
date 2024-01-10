5e9d34fe3a2ffd5ad9d16236ec57ed8a738e70e6
A252422 <A252422@LSBSA-19Y5HW3>
2024-01-10 12:51:14 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16.Exception_Handling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter a number");
+                int num1 = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter another number");
+                int num2 = int.Parse(Console.ReadLine());
+                Console.WriteLine("The division result is  " + num1 / num2);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.Read();
+        }
+    }
+}
